bc65127db5b162524b510e6d3fa2a2e79f32a698
gaurav3278 <gaurav.rai2020@vitbhopal.ac.in>
2022-06-19 18:32:23 +0530

    final

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,16 @@
 import Theme from '../styles/theme';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>My Portfolio</title>
+        <link rel='icon' href='https://img.icons8.com/fluency/48/undefined/portfolio.png' type='image/x-icon' />
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
     </>
   );
 }
- 
